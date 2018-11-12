efc78164ff2307ed5dffc7afb1e04c33088dcde6
Xindi Xu <xindixu@utexas.edu>
2018-11-12 02:45:45 -0600

    add profile page / fetch data from database

--- angular-src/src/app/pages/profile/profile.component.ts
+++ angular-src/src/app/pages/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user: Object;
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.authService.getProfile().subscribe(profile => {
+      this.user = profile.user;
+    },
+    err => {
+      console.log(err);
+      return false;
+    });
+
   }
 
 }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -23,6 +23,20 @@ export class AuthService {
       .pipe(map(res => res.json()));
   }
 
+  getProfile(){
+    this.loadToken();
+    let headers = new Headers();
+    headers.append('Authorization', this.authToken);
+    headers.append('Content-Type', 'application/json');
+    return this.http.get('http://localhost:3000/users/profile', { headers: headers })
+      .pipe(map(res => res.json()));
+  }
+
+  loadToken(){
+    const token = localStorage.getItem('id_token');
+    this.authToken = token;
+  }
+
   storeUserData(token, user){
     localStorage.setItem('id_token', token);
     // localStorage only stores stings

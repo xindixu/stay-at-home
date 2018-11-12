bcdf5504795081e1467fe5804d8e26f352e18cd2
Xindi Xu <xindixu@utexas.edu>
2018-11-12 15:51:24 -0600

    add db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/stay-at-home',
+  //database: 'mongodb://localhost:27017/stay-at-home',
+  database:'mongodb://xindixu:xindixu1234@ds159993.mlab.com:59993/stay-at-home',
   secret: 'capser stole arby\'s food'
 };
